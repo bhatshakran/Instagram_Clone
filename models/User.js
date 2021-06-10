f3d03106badd77d03f48b77532a1fcbf578c90b5
shaqran_bhat <bhatshakran@gmail.com>
2021-06-10 16:50:28 +0530

    password hashing added using bcryptjs

--- models/User.js
+++ models/User.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
   password: {
-    type: password,
+    type: String,
     required: true,
   },
 });
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
 
 // @route POST api/users
 //  @desc Register User
@@ -22,8 +24,43 @@ router.post(
       return res.status(400).json({ Errors: errors.array() });
     } else {
       const { name, email, password } = req.body;
+
+
+    //   Check if user exists with that email
       try {
-        res.json({ name, email, password });
+        let user = await User.findOne({ email })
+        if (user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "User already exists with that email" }] })
+        }
+        // Create new user with that email
+        user = new User({
+            name, 
+            email, 
+            password
+        })
+
+        // We dont want plain text passwords
+        // So we will hash them using bcrypt
+        
+        const salt = await bcrypt.genSalt(10)
+
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save()
+
+        res.status(200).json({"message":"User registered"});
+
+
+
+        
+
+
+
+
+
+
       } catch (err) {
         console.error(err.message);
         res.status(500).send("Server error");
