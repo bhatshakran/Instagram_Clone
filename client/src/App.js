9a790ffc4869d81ca92abd17903aa4582e9a58f6
shaqran_bhat <bhatshakran@gmail.com>
2021-06-11 20:09:23 +0530

    Initiated react frontend with tailwindcss

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className=" text-blue-500">HEY yo man jus checking</div>
+    </div>
+  );
+}
+
+export default App;
