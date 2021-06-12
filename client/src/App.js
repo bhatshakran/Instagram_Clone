04b4c873f1c5bac82b9507908d2dbbe29c3489e4
shaqran_bhat <bhatshakran@gmail.com>
2021-06-12 07:15:52 +0530

    Post component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Homepage from "./components/Homepage";
 import Navbar from "./components/Layout/Navbar";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
@@ -9,6 +10,7 @@ function App() {
     <div className="mx-auto App lg:container">
       <Navbar />
       <Switch>
+        <Route exact path="/" component={Homepage} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
       </Switch>
---
+++ client/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Postcard from "./Postcard";
+
+const Homepage = () => {
+  return (
+    <div className="mx-auto mt-5 maincontainer">
+      <Postcard />
+    </div>
+  );
+};
+
+export default Homepage;
