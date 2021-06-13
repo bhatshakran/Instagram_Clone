003d0379be4dbd5568f295a1efbc0b2b845fd370
shaqran_bhat <bhatshakran@gmail.com>
2021-06-13 19:37:00 +0530

    Modified serverjs

--- server.js
+++ server.js
@@ -17,21 +17,17 @@ app.use("/api/auth", require("./routes/api/auth"));
 // Posts routing
 app.use("/api/posts", require("./routes/api/posts"));
 
-// Serve static assets
-if(process.env.NODE_ENV === 'production'){
+// Serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    );
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
-
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log("Server is runnning on port " + PORT));
