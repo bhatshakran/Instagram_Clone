775b41ce55cc8afe92c256490db9845e801ce3f0
shaqran_bhat <bhatshakran@gmail.com>
2021-06-13 17:44:28 +0530

    Pushing to heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path");
 
 // Connect Database
 connectDB();
@@ -16,9 +17,18 @@ app.use("/api/auth", require("./routes/api/auth"));
 // Posts routing
 app.use("/api/posts", require("./routes/api/posts"));
 
-app.get("/", (req, res) => {
-    res.send("Home page");
-});
+// Serve static assets
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.user(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
+
+
 
 const PORT = process.env.PORT || 5000;
 
