5cf8e655bf3761cbe750f50b385171dbde579898
shaqran_bhat <bhatshakran@gmail.com>
2021-06-14 19:33:11 +0530

    Added loading functionality

--- client/src/components/Posts/CreatePost.js
+++ client/src/components/Posts/CreatePost.js
@@ -1,31 +1,45 @@
 import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import {Link} from 'react-router-dom'
-import {  hideAlert } from "../../utils/hideAlert";
+import { hideAlert } from "../../utils/hideAlert";
 import AddpostForm from "../Forms/AddpostForm";
+import Loading from "../../utils/Loading";
+
 
 const CreatePost = () => {
+  const loading = useSelector(state => state.posts.loading)
   let alertRef = useRef(null)
   const alert = useSelector((state) => state.posts.alertMessage);
-  console.log(alert);
+  console.log(loading)
 
-  
-  return (
-    <div className="h-screen ">     
-      <div className="mx-auto mt-8 border border-gray-200 card">
-      {alert !== "" ? (
-        <div ref={alertRef} className="py-4 text-center text-white bg-blue-300">
-          {alert}{' '}
-          {hideAlert(alertRef)} 
-          <Link to='/' className='text-black '>View Posts</Link>
-          </div>
-      ) : (
-        ""
-      )}
-        <AddpostForm />
+  if(loading){
+   return  <div className="flex items-center justify-center h-screen ">
+  <Loading />
+  </div>
+  }else{
+    return (
+      <div className="h-screen ">
+        <div className="mx-auto mt-8 border border-gray-200 card">
+          {alert !== "" ? (
+            <div
+              ref={alertRef}
+              className="py-4 text-center text-white bg-blue-300"
+            >
+              {alert} {hideAlert(alertRef)}
+              <Link to="/" className="text-black ">
+                View Posts
+              </Link>
+            </div>
+          ) : (
+            ""
+          )}
+          <AddpostForm />
+          
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 };
 
 export default CreatePost;
---
+++ client/src/utils/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import rocket from "../images/gifs/Rocket.gif";
+
+const Loading = () => {
+  return (
+    <div>
+      <img src={rocket} alt="" />
+    </div>
+  );
+};
+
+export default Loading;
