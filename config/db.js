6a0eec72d337a96bfca5ec3a809e3a67a2d09a9f
shaqran_bhat <bhatshakran@gmail.com>
2021-06-15 20:11:55 +0530

    Changed mongouri

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+// const config = require("config");
+// const db = config.get("mongoURI");
 require("dotenv").config();
 // const uri = process.env.mongoURI;
 
@@ -8,10 +8,13 @@ require("dotenv").config();
 // 8LSO18Oh2G3gE0qU
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://shaqran:Munazah668@cluster0.1tc8t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MongoDB connected...");
   } catch (err) {
     console.error(err.message);
