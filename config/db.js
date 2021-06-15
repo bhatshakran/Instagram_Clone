6ede6b13bb4fa28f1ceb3f3dbc1f4851846a931f
shaqran_bhat <bhatshakran@gmail.com>
2021-06-15 19:45:08 +0530

    pushing to heroku

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
-// const uri = process.env.mongoURI;
+// const config = require("config");
+// const db = config.get("mongoURI");
+const uri = process.env.mongoURI;
 
 
 // 8LSO18Oh2G3gE0qU
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
