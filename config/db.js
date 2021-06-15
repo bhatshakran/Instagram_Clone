1010e3229873fb2a1e7fb7c33a41600293a91e20
shaqran_bhat <bhatshakran@gmail.com>
2021-06-15 19:57:43 +0530

    changes in db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-// const config = require("config");
-// const db = config.get("mongoURI");
+const config = require("config");
+const db = config.get("mongoURI");
 // require("dotenv").config();
 const uri = process.env.mongoURI;
 
