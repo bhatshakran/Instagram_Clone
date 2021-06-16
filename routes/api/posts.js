7fcfe2defb60c0aa7fb3ac3ef2bdc2a9edec1e46
shaqran_bhat <bhatshakran@gmail.com>
2021-06-16 18:21:31 +0530

    Created route handler for post comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -106,10 +106,12 @@ router.put("/like/:id", auth, async (req, res) => {
       return res.status(400).json("Post has already been liked!");
     }
 
-    post.likes.push({ user: req.user.id });
+    post.likes.unshift({ user: req.user.id });
 
     await post.save();
     res.json(post.likes);
+    
+    
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
@@ -134,9 +136,7 @@ router.put("/unlike/:id", auth, async (req, res) => {
     }
 
     // Get remove index
-    console.log(req.user);
     const removeIndex = post.likes.map((like) =>{
-         console.log(like.user)
       like.user.toString().indexOf(req.user.id)
     }
     );
@@ -152,5 +152,37 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route PUT api/posts/comment/:id
+// @desc Coment on a post
+// @access Private
+router.post("/comment/:id", [auth,
+[check("text", "Text is required").not().isEmpty()]], async(req, res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array() })
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    const post = await Post.findById(req.params.id);
+
+
+    const newComment = {
+      text:req.body.text,
+      name:user.name,
+      user:req.user.id
+    }
+
+    post.comments.unshift(newComment)
+    await post.save()
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error')
+  }
+
+})
 
 module.exports = router;
