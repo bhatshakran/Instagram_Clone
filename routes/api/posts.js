7e3a6239fc4146a9967b6d3f8203d9cd6271b75a
shaqran_bhat <bhatshakran@gmail.com>
2021-06-17 17:07:26 +0530

    Made delete reducer and action

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -197,5 +197,44 @@ router.get("/comment/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+// @route DELETE api/posts/comment/:id
+// @desc Delete a comment
+// @access Private
+router.delete('/post/:id/:comment/:comment_id', auth, async (req, res) => {
+  try {
+    // Get the post first
+    const post = await Post.findById(req.params.id);
+    
+    // Pull out the comment
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+
+
+    // Make sure comment exists
+    if(!comment){
+      return res.status(404).json({msg:'Comment does not exist'})
+    }
+
+    // Check user
+    if(comment.user.toString() !== req.user.id){
+      return res.status(400).json({msg: "User not authorized to perform this action!"})
+    }
+
+    // Get remove index
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id)
+
+
+    // Splice the array
+    posts.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+    
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+})
+
 
 module.exports = router;
