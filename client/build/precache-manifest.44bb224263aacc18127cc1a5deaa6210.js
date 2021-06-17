a9c60a625485fe620b01cc09be1a93bae4e0e972
shaqran_bhat <bhatshakran@gmail.com>
2021-06-17 15:33:33 +0530

    Updated react scripts

---
+++ client/build/precache-manifest.44bb224263aacc18127cc1a5deaa6210.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a139184ee5e0ee42f3b7",
+    "url": "/static/css/main.66719fb6.chunk.css"
+  },
+  {
+    "revision": "a139184ee5e0ee42f3b7",
+    "url": "/static/js/main.ae6c87e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b0142f41750f1a15a65f",
+    "url": "/static/js/2.1e376c57.chunk.js"
+  },
+  {
+    "revision": "cfa70481f7385058708abf5d8e59ff29",
+    "url": "/static/media/Rocket.cfa70481.gif"
+  },
+  {
+    "revision": "bed171f38bc538c188efa49f41318255",
+    "url": "/static/media/heart.bed171f3.png"
+  },
+  {
+    "revision": "95f2338a8ddd7c2bbff691f066a04686",
+    "url": "/static/media/heart_filled.95f2338a.png"
+  },
+  {
+    "revision": "656af481791b3530297621d242d513ac",
+    "url": "/index.html"
+  }
+];
