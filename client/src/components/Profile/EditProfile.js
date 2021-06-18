20d15d796e71b216a8b81d3bf78ce1222dd9f785
shaqran_bhat <bhatshakran@gmail.com>
2021-06-18 19:06:30 +0530

    Edit profile responsiveness done

--- client/src/components/Profile/EditProfile.js
+++ client/src/components/Profile/EditProfile.js
@@ -3,9 +3,9 @@ import shore from "../../images/shore.jpeg";
 
 const EditProfile = () => {
   return (
-    <div className="min-h-screen pt-6 mt-8 border-t">
+    <div className="min-h-screen pt-6 mt-8 border-t md:border-t-0 md:w-2/3 md:mx-auto">
       {/* image updation */}
-      <div className="grid items-center grid-cols-9 image_editing">
+      <div className="grid items-center grid-cols-9 image_editing ">
         <div className="px-3 image">
           <img src={shore} alt="" className="w-10 h-10 rounded-full" />
         </div>
@@ -16,14 +16,16 @@ const EditProfile = () => {
           </button>
         </div>
       </div>
-      <div className="max-w-sm">
+      <div className="w-3/4 ">
         {/* name updation */}
-        <div className="grid grid-cols-1 mx-4 mt-8 name_edit">
-          <label htmlFor="name" className="font-medium text-md">
+        <div className="grid grid-cols-1 mx-4 mt-8 name_edit md:grid-cols-6">
+            
+          <label htmlFor="name" className="font-medium text-md md:col-start-1 md:col-end-2">
             Name
           </label>
-          <input type="text" name="name" id="" className="py-1 border" />
-          <p className="mt-4 leading-3 text-gray-500">
+          <input type="text" name="name" id="" className="py-1 border md:col-start-2 md:col-end-7" />
+         
+          <p className="mt-4 leading-3 text-gray-500 md:col-start-1 md:col-end-7">
             <small>
               Help people discover your account by using the name you're known
               by: either your full name, nickname, or business name.
@@ -33,12 +35,12 @@ const EditProfile = () => {
           </p>
         </div>
         {/* username updation */}
-        <div className="grid grid-cols-1 mx-4 mt-8 name_edit">
-          <label htmlFor="username" className="font-medium text-md">
+        <div className="grid grid-cols-1 mx-4 mt-8 name_edit md:grid-cols-6 md:gap-x-20">
+          <label htmlFor="username" className="font-medium text-md md:col-start-1 md:col-end-2">
             Username
           </label>
-          <input type="text" name="username" id="" className="py-1 border" />
-          <p className="mt-4 leading-3 text-gray-500">
+          <input type="text" name="username" id="" className="py-1 border md:col-start-2 md:col-end-7" />
+          <p className="mt-4 leading-3 text-gray-500 md:col-start-1 md:col-end-7">
             <small>
               In most cases, you'll be able to change your username back to
               shakran._.bhat for another 14 days. Learn More
@@ -46,23 +48,23 @@ const EditProfile = () => {
           </p>
         </div>
         {/* website */}
-        <div className="grid grid-cols-1 mx-4 mt-8 name_edit">
-          <label htmlFor="webiste" className="font-medium text-md">
+        <div className="grid grid-cols-1 mx-4 mt-8 name_edit md:grid-cols-6">
+          <label htmlFor="webiste" className="font-medium text-md md:col-start-1 md:col-end-2">
             Website
           </label>
-          <input type="text" name="website" id="" className="py-1 border" />
+          <input type="text" name="website" id="" className="py-1 border md:col-start-2 md:col-end-7" />
         </div>
         {/* Bio */}
-        <div className="grid grid-cols-1 mx-4 mt-8 name_edit">
-          <label htmlFor="bio" className="font-medium text-md">
+        <div className="grid grid-cols-1 mx-4 mt-8 name_edit md:grid-cols-6">
+          <label htmlFor="bio" className="font-medium text-md md:col-start-1 md:col-end-2">
             Bio
           </label>
-          <textarea name="bio" id="" className="py-1 border"></textarea>
+          <textarea name="bio" id="" className="py-1 border md:col-start-2 md:col-end-7"></textarea>
         </div>
         {/* Personal Information */}
         <div className="grid grid-cols-1 mx-4 mt-8 name_edit">
           <h3 className="text-gray-500 ">Personal Information</h3>
-          <p className="mt-2 leading-3 text-gray-500">
+          <p className="mt-2 leading-3 text-gray-500 md:col-start-1 md:col-end-7">
             <small>
               Provide your personal information, even if the account is used for
               a business, a pet or something else. This won't be a part of your
@@ -71,25 +73,25 @@ const EditProfile = () => {
           </p>
         </div>
         {/* Email and phone number*/}
-        <div className="grid grid-cols-1 mx-4 mt-8 name_edit">
-          <label htmlFor="email" className="font-medium text-md">
+        <div className="grid grid-cols-1 gap-2 mx-4 mt-8 name_edit md:grid-cols-6 md:items-center">
+          <label htmlFor="email" className="font-medium text-md md:col-start-1 md:col-end-2">
             Email
           </label>
-          <input type="text" name="email" id="" className="py-1 border" />
+          <input type="text" name="email" id="" className="py-1 border md:col-start-2 md:col-end-7" />
 
-          <label htmlFor="phone" className="mt-3 font-medium text-md">
+          <label htmlFor="phone" className="mt-3 font-medium text-md md:col-start-1 md:col-end-2">
             Phone Number
           </label>
 
-          <input type="text" name="phone" id="" className="py-1 border" />
+          <input type="text" name="phone" id="" className="py-1 border md:h-2/3 md:col-start-2 md:col-end-7" />
 
-          <label htmlFor="gender" className="mt-3 font-medium text-md">
+          <label htmlFor="gender" className="mt-3 font-medium text-md md:col-start-1 md:col-end-2">
             Gender
           </label>
-          <input type="text" name="gender" id="" className="py-1 border" />
+          <input type="text" name="gender" id="" className="py-1 border md:col-start-2 md:col-end-7" />
         </div>
         {/* Similar account suggestions */}
-        <div className="mx-4 mt-8 mb-3 name_edit">
+        <div className="mx-4 mt-8 mb-3 name_edit md:grid-cols-6">
           <button className="px-3 py-1 text-sm font-medium text-white bg-blue-300 rounded">
             Submit
           </button>
